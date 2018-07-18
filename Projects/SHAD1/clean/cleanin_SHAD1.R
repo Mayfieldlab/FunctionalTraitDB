36e1c9115ee1ea114a28c9a106a9c26b9a9e5e42
cathbowler <39176978+cathbowler@users.noreply.github.com>
2018-07-18 10:01:33 +1000

    Cath changes
    Updated cleaning script, added readme's, created clean csv's for projects EUCD1, GERM1, & SHAD1.

--- Projects/SHAD1/clean/cleanin_SHAD1.R
+++ Projects/SHAD1/clean/cleanin_SHAD1.R
@@ -9,8 +9,8 @@
 
 # Set path to project folder
 ############################
-local_path <- '~/Dropbox/' # this is where you put in the path to whatever folder holds the Mayfield Lab folder on your computer
-path <- paste0(local_path, 'Mayfield Lab/FunctionalTraitDatabase/Projects/SHAD1/')
+local_path <- '~/Documents/' # this is where you put in the path to whatever folder holds the Mayfield Lab folder on your computer
+path <- paste0(local_path, 'GitHub/FunctionalTraitDB/Projects/SHAD1/')
 
 # Load relevant files 
 #######################
@@ -31,15 +31,16 @@ treatment_id <-
 # Now on to the output 
 ########################
 
-# projects table  // UNFINISHED //
+# projects table  
 #----------------
 projects <- data.frame(project_id = project_id,
+                       project = 'WA shade experiment',
                        researcher = 'ClaireWainwright',
                        email = 'cewain@u.washington.edu',
-                       study_sites = '')
+                       study_sites = 'Perenjori')
 write.csv(projects, paste0(path, 'clean/projects.csv'), row.names = F)
 
-# field season table  // UNFINISHED //
+# field season table 
 #--------------------
 field_season <- data.frame(field_season_id = field_season_id,
                            project_id = project_id,
@@ -50,6 +51,33 @@ field_season <- data.frame(field_season_id = field_season_id,
                            fs_mean_precip_mm = NA)   # not available in the collected data
 write.csv(field_season, paste0(path, 'clean/field_season.csv'), row.names = F)
 
+# site table UNFINISHED 
+#-------------------
+site <- data.frame(site_id = site_id,
+                   site = 'West Perenjori Nature Reserve',
+                   area = 3.6,
+                   units = 'km2',
+                   lat = ,
+                   max_temp_C = ,
+                   min_temp_C = ,
+                   annual_temp_mm = )
+write.csv(site, paste0(path, 'clean/site.csv'), row.names = F)
 
+# treatment table 
+#------------------
+treatment <- data.frame(treatment_id = treatment_id, 
+                        treatment = 'shade, open')
+write.csv(treatment, paste0(path, 'clean/treatment.csv'), row.names = F)
 
+# trait summary table - not even sure if all of these are traits?
+#-------------------
+trait_summary <- data.frame(field_season_id = field_season_id,
+                            traits = Focal.height_Solo.height_Height_Num.fl.total_Num.fl.seeding.total_Seedcount.extrap.interger_Mean.solo.seedcount.extrap.integer.block_Delta.seedcount.meansolospeciesblock_Solo.seedcount.extrap.integer_Delta.seedcount.highcomp.solo.pair_no.developed.flowers_Mean.seed.per.flower_Extrapolated.Seed.Total_Viability_Germination)
+write.csv(trait_summary, paste0(path, 'clean/trait_summary.csv'), row.names = F)
 
+# plot table 
+#------------------
+plot <- data.frame(plot_id = plot_id,
+                   field_season_id = field_season_id,
+                   site_id = site_id,
+                   block = )
